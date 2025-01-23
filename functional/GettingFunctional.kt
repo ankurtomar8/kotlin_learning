34c389aae03ff55357cead3ca00a51b4bf3a056f
AnkurDev <ankurtomar8@outlook.com>
2025-01-23 08:56:27 +0530

    [Kotlin_Oreilly] GettingFunctional in kotlin added to understand kotlin
    [Desc] Updated GettingFunctional.kt to understand Kotlin language
    [Sprint] Day6
    [Scope] New
    
    Fix NA

--- functional/GettingFunctional.kt
+++ functional/GettingFunctional.kt
@@ -1,6 +1,20 @@
 package functional
 
 
+fun transaction(db: Database,code: () -> Unit){
+   try {
+       code()
+   }finally {
+       db.commit()
+   }
+}
+
+class Database {
+   fun commit() {
+
+   }
+}
+
 fun operation(x:Int, y: Int, op: (Int,Int) -> Int):Int {
    return op(x,y)
 }
@@ -19,6 +33,16 @@ fun main(){
    println(unaryOperation(2,{ x -> x * x }))
 
    println(unaryOperation(3,{ it * it }))
+
+   println(unaryOperation(3) { it * it })
+
+
+   val db = Database()
+
+   transaction(db) {  /// Creating own keyword DSL transaction more meaning
+      // interact with database
+   }
+
 }
 
 class GettingFunctional {

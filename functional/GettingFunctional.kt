7e84e6e72e4f66a3ccc683b5c4a538ca1d54e13d
AnkurDev <ankurtomar8@outlook.com>
2025-01-23 08:45:36 +0530

    [Kotlin_Oreilly] GettingFunctional in kotlin added to understand kotlin
    [Desc] Updated GettingFunctional.kt to understand Kotlin language
    [Sprint] Day6
    [Scope] New
    
    Fix NA

--- functional/GettingFunctional.kt
+++ functional/GettingFunctional.kt
@@ -7,9 +7,18 @@ fun operation(x:Int, y: Int, op: (Int,Int) -> Int):Int {
 
 fun sum (x: Int , y: Int) = x+y
 
+fun unaryOperation(x:Int, op: (Int) -> Int){
+
+}
+
 fun main(){
    println( operation(1,2, ::sum) ) // Passing sum function to operation function rather then storing values
      //
+   println(""+  operation(4,2,{x,y -> x+y})) // Lambda expression
+
+   println(unaryOperation(2,{ x -> x * x }))
+
+   println(unaryOperation(3,{ it * it }))
 }
 
 class GettingFunctional {

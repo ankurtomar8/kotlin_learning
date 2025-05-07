731aa3d76c785ac886293843993e80a4ad57c43c
AnkurDev <ankurtomar8@outlook.com>
2025-05-08 00:30:27 +0530

    [Problems] New Added Revision added
    [Topic] New Added

---
+++ _01_intermediate_refresher/refresher_introduction_to_java_input_output_data_types_operators/Q7SimpleMaths.kt
@@ -0,0 +1,8 @@
+package _01_intermediate_refresher.refresher_introduction_to_java_input_output_data_types_operators
+
+fun main(){
+    println(6+3)
+    println(6-3)
+    println(6*3)
+    println(6.0/3.0)
+}

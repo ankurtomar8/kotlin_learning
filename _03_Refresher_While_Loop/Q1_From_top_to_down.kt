077be4831b16e67a3d89cb4db4573d74c7892de2
AnkurDev <ankurtomar8@outlook.com>
2025-05-10 18:22:24 +0530

    [Topic] IF-ELSE  [Problems] Name: Q2. From down to top[Level]Easy/Medium

--- _03_Refresher_While_Loop/Q1_From_top_to_down.kt
+++ _03_Refresher_While_Loop/Q1_From_top_to_down.kt
@@ -1,8 +1,8 @@
 fun main(){
     var  n = readLine()!!.toInt()
-    var i = 1
-    while( i <= n){
-        print("${i} ")
-        i++
+    
+    while(n >= 1){
+        print("${n} ")
+        n--
     }
 }
